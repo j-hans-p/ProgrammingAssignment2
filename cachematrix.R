93f930e8d79f7b16692c8e54471ea6840550809b
BearCatBoston <jeffrey.peo@gmail.com>
2020-03-28 01:12:45 -0400

    Updated provided code to retrieve cached inverse of matrix instead of mean of vector

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache the value of the inverse of a matrix so that when it's needed
+## again it can be looked up in the cache rather than recalcuated.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" and uses the <<- operator to cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() 
+    x
+  setinverse <- function(solve) 
+    m <<- inverse
+  getinverse <- function() 
+    m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## this function retrieves the calculated inverse of the matrix from the cache if 
+##the matrix has not changed
+## if the matrix has changed, it computes the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+  
 }
